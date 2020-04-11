4cdcba2d868efa831d3d845a749aacddea9947a7
RemyVase <remy.vase3@hotmail.fr>
2020-04-11 19:40:55 +0200

    Résolution du bug lorsqu'on envoyait un message d'une assoc à un user avec assoc

--- flash/controller/envoiMessageAssocToUserController.php
+++ flash/controller/envoiMessageAssocToUserController.php
@@ -10,11 +10,29 @@ $message = $_POST['message'];
 
 $checkUserConv = $db->callProcedure('messageCheckUserConv', [$_SESSION['idAssoc'],$idReceveur]);
 
-if (empty($checkUserConv)) {
-    $conversation = $db->callProcedure('messageCreateConvers',[$_SESSION['idAssoc']]);
-    $idConvers = $db->callProcedure('messageTakeLastConvCree');
-    $lierConversation = $db->callProcedure('messageLierConversation', [$idReceveur, intval($idConvers[0]{'id_convers'})]);
-    $envoiMessage = $db->callProcedure('messageEnvoiMessage', [$idEnvoyeur, $idConvers[0]{'id_convers'}, $message]);
-} else {
-    $envoiMessage = $db->callProcedure('messageEnvoiMessage', [$idEnvoyeur, intval($checkUserConv[0]{'id_convers'}), $message]);
+$checkAssocUser = $db->callProcedure('messageCheckSiEnvoyeurDansAssoc',[$idReceveur]);
+
+$checkAssocToAssocConv = $db->callProcedure('messageCheckAssocToAssocConv', [$_SESSION['idAssoc'], $checkAssocUser[0]{'id_assoc'}]);
+
+if($checkAssocUser[0]{'id_assoc'} === NULL){
+    if (empty($checkUserConv)) {
+        $conversation = $db->callProcedure('messageCreateConvers',[$_SESSION['idAssoc']]);
+        $idConvers = $db->callProcedure('messageTakeLastConvCree');
+        $lierConversation = $db->callProcedure('messageLierConversation', [$idReceveur, intval($idConvers[0]{'id_convers'})]);
+        $envoiMessage = $db->callProcedure('messageEnvoiMessage', [$idEnvoyeur, $idConvers[0]{'id_convers'}, $message]);
+    } else {
+        $envoiMessage = $db->callProcedure('messageEnvoiMessage', [$idEnvoyeur, intval($checkUserConv[0]{'id_convers'}), $message]);
+    }
+}else{
+    if(empty($checkAssocToAssocConv)){
+        $conversation = $db->callProcedure('messageCreateConvers', [$checkAssocToAssocConv[0]{'id_assoc'}]);
+        $idConvers = $db->callProcedure('messageTakeLastConvCree');
+        $lierConversation = $db->callProcedure('messageLierConversationToAssoc', [$_SESSION['idAssoc'], intval($idConvers[0]{
+            'id_convers'})]);
+        $envoiMessage = $db->callProcedure('messageEnvoiMessage', [$idEnvoyeur, $idConvers[0]{
+            'id_convers'}, $message]);
+    } else {
+        $envoiMessage = $db->callProcedure('messageEnvoiMessage', [$idEnvoyeur, intval($checkAssocToAssocConv[0]{
+            'id_convers'}), $message]);
+    }
 }
